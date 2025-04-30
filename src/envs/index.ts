1fa26847aa97a5865721b2f40f7e443a888e5406
kevinand11 <kevinfizu@gmail.com>
2025-04-30 14:24:38 +0100

    envs ns

--- src/envs/index.ts
+++ src/envs/index.ts
@@ -1,6 +1,6 @@
 import { K8sConfig } from '@devops/k8s-cdk/k8s'
 
-const namespace = 'envs'
+const namespace = 'stranerd-envs'
 
 export const devopsConfig = K8sConfig.of({ name: 'stranerd.devops', namespace, secret: true })
 export const apiConfig = K8sConfig.of({ name: 'stranerd.api', namespace, secret: true })
